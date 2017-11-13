1788841cc51a26f4d077887d46f1e1ce89a1aa75
尹吉欢 <yinjihuan@fangjia.com>
2017-11-13 17:20:21 +0800

    add comment

--- fangjia-api-client/src/main/java/com/fangjia/api/client/config/FeignConfiguration.java
+++ fangjia-api-client/src/main/java/com/fangjia/api/client/config/FeignConfiguration.java
@@ -7,6 +7,10 @@ import feign.Logger;
 
 @Configuration
 public class FeignConfiguration {
+    /**
+     * 日志级别
+     * @return
+     */
 	@Bean  
     Logger.Level feignLoggerLevel() {  
         return Logger.Level.FULL;  
