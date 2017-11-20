1a3d5f9f4d7536fefd5059d515ad9a76f0d4d07d
尹吉欢 <yinjihuan@fangjia.com>
2017-11-20 16:53:07 +0800

    增加Swagger

--- fangjia-fsh-house-service/src/main/java/com/fangjia/fsh/house/FshHouseServiceApplication.java
+++ fangjia-fsh-house-service/src/main/java/com/fangjia/fsh/house/FshHouseServiceApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
@@ -14,6 +15,7 @@ import org.springframework.context.annotation.ComponentScan;
  */
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableHystrix
 @EnableFeignClients
 @ComponentScan(basePackages={"com.fangjia"})
 public class FshHouseServiceApplication {
