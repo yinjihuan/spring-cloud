d45d4e65793baa8c6d351a09fe98384ba03bc0e3
尹吉欢 <yinjihuan@fangjia.com>
2017-11-28 16:11:36 +0800

    add spring boot admin

---
+++ fangjia-boot-admin/src/main/java/com/fangjia/admin/AdminApplication.java
@@ -0,0 +1,23 @@
+package com.fangjia.admin;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 程序监控
+ *
+ * @author yinjihuan
+ * @create 2017-11-28 15:26
+ **/
+@Configuration
+@EnableAutoConfiguration
+@EnableDiscoveryClient
+@EnableAdminServer
+public class AdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApplication.class, args);
+    }
+}
---
+++ fangjia-boot-admin/src/main/java/com/fangjia/admin/SecurityConfig.java
@@ -0,0 +1,34 @@
+package com.fangjia.admin;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * 安全配置
+ *
+ * @author yinjihuan
+ * @create 2017-11-28 15:41
+ **/
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // Page with login form is served as /login.html and does a POST on /login
+        http.formLogin().loginPage("/login.html").loginProcessingUrl("/login").permitAll();
+        // The UI does a POST on /logout on logout
+        http.logout().logoutUrl("/logout");
+        // The ui currently doesn't support csrf
+        http.csrf().disable();
+
+        // Requests for the login page and the static assets are allowed
+        http.authorizeRequests()
+                .antMatchers("/login.html", "/**/*.css", "/img/**", "/third-party/**")
+                .permitAll();
+        // ... and any other request needs to be authorized
+        http.authorizeRequests().antMatchers("/**").authenticated();
+
+        // Enable so that the clients can authenticate via HTTP basic for registering
+        http.httpBasic();
+    }
+}
