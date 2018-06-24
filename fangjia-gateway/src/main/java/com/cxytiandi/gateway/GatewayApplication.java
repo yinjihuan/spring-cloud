9e3d7839f5aa70623dac8c04dfe40a5f4c1c8d9f
yinjihuan <jihuan900@126.com>
2018-06-24 17:03:19 +0800

    commit gateway code

---
+++ fangjia-gateway/src/main/java/com/cxytiandi/gateway/GatewayApplication.java
@@ -0,0 +1,32 @@
+package com.cxytiandi.gateway;
+
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+/**
+ * 网关启动入口
+ * 
+ * @author yinjihuan
+ * 
+ * @about http://cxytiandi.com/about
+ *
+ */
+@SpringBootApplication
+public class GatewayApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+	
+	//@Bean
+	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+	    return builder.routes()
+	            .route(r -> r.path("/course").uri("http://cxytiandi.com"))
+	            .build();
+	}
+	
+}
