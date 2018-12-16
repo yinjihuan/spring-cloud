c9fe9d600224ee2c21830419be10b18ad7bd35bd
yinjihuan <jihuan900@126.com>
2018-12-16 22:00:57 +0800

    commit spring boot admin code

---
+++ ch-11/spring-boot-admin-client/src/main/java/com/cxytiandi/spring_boot_admin_client/App.java
@@ -0,0 +1,22 @@
+package com.cxytiandi.spring_boot_admin_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * Spring Boot Admin Client端示列
+ * 
+ * @author yinjihuan
+ * 
+ * @about http://cxytiandi.com/about
+ * 
+ * @date 2018-12-16
+ * 
+ */
+@SpringBootApplication
+public class App {
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+}
