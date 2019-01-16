fc937858861cac3ad36c426fd040f37bcef14926
yinjihuan <jihuan900@126.com>
2019-01-16 21:49:00 +0800

    commit elastic job xml config code

---
+++ Spring-Cloud-Book-Code-2/ch-19/elastic-job-demo/src/main/java/com/cxytiandi/job/MessageElasticJobListener.java
@@ -0,0 +1,29 @@
+package com.cxytiandi.job;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import com.dangdang.ddframe.job.executor.ShardingContexts;
+import com.dangdang.ddframe.job.lite.api.listener.ElasticJobListener;
+
+/**
+ * 作业监听器, 执行前后发送钉钉消息进行通知
+ * @author yinjihuan
+ */
+public class MessageElasticJobListener implements ElasticJobListener {
+
+    @Override
+    public void beforeJobExecuted(ShardingContexts shardingContexts) {
+        String date = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
+        String msg = date + " 【猿天地-" + shardingContexts.getJobName() + "】任务开始执行====" + JsonUtils.toJson(shardingContexts);
+        DingDingMessageUtil.sendTextMessage(msg);
+    }
+
+    @Override
+    public void afterJobExecuted(ShardingContexts shardingContexts) {
+    	String date = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
+        String msg = date + " 【猿天地-" + shardingContexts.getJobName() + "】任务执行结束====" + JsonUtils.toJson(shardingContexts);
+        DingDingMessageUtil.sendTextMessage(msg);
+    }
+
+}
